7f868a4f7463bb42082756f56efb6b312f1a4bde
hardcorebuddhist <im.energo@gmail.com>
2022-05-16 00:41:11 +0200

    new commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const SampleRouter = require("./routers/sampleRouter");
+const cors = require("cors");
 
 const PORT = 4000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/samples", SampleRouter);
 
--- routers/sampleRouter.js
+++ routers/sampleRouter.js
@@ -2,11 +2,28 @@ const { Router } = require("express");
 const router = new Router();
 const Sample = require("../models").sample;
 
+// All samples
+
 router.get("/", async (req, res, next) => {
   try {
     res.send(await Sample.findAll());
   } catch (e) {
     console.log(e);
+    next(e);
+  }
+});
+
+// DRUM SAMPLES
+
+router.get("/drums", async (req, res, next) => {
+  try {
+    const drumSamples = await Sample.findAll({ where: { type: "drums" } });
+    if (drumSamples) {
+      res.send(drumSamples);
+    } else {
+      res.status(404).send("Sample not found");
+    }
+  } catch (e) {
     next(e);
   }
 });
